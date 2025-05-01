2e5e4b973dda8cdc6adf92a83d1636c350e1d062
daraghkearney <150018600+daraghkearney@users.noreply.github.com>
2025-05-01 15:54:42 +0100

    Testing secrets.h

---
+++ secrets.h
@@ -0,0 +1,7 @@
+// Daragh Kearney 2025 //
+// secrets.h //
+
+#define SECRET_SSID "SKYHGSM1"
+#define SECRET_PASS "yDtPgkUALtAR"
+#define SECRET_CH_ID 2713003
+#define SECRET_WRITE_APIKEY "6CPF9H3D5KCSZGFT"
